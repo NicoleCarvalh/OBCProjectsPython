0a46622dd46fbc0eb7b175bf32a0b43f172dca98
NicoleCarvalh <nicolecarvalho6@gmail.com>
2024-04-28 09:23:23 -0300

    First commit

---
+++ PYTHON/5-exercicio1.py
@@ -0,0 +1,10 @@
+num = int(input("Escreva aqui um número:"))
+
+print(f"O antecessor desse número é: {num-1} e o sucessor desse número é: {num+1}")
+
+grade1 = int(input("Insira a nota 1: "))
+grade2 = int(input("Insira a nota 2: "))
+grade3 = int(input("Insira a nota 3: "))
+grade4 = int(input("Insira a nota 4: "))
+
+print(f"Média final: {(grade1 + grade2 + grade3 + grade4) / 4}")
